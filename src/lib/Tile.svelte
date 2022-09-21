54f0ebfe0765e3fa716d129c1884407a30f962f4
Neil <barrymun@tcd.ie>
2022-09-21 16:08:11 +0100

    [fix] checkers should be on dark squares

--- src/lib/Tile.svelte
+++ src/lib/Tile.svelte
@@ -4,8 +4,8 @@
 
   const tileColor: string =
     (x % 2 == 0 && y % 2 == 0) || (x % 2 != 0 && y % 2 != 0)
-      ? "bg-green-100"
-      : "bg-green-300";
+      ? "bg-green-300"
+      : "bg-green-100";
 
   const onDragOver = (e: DragEvent) => {
     e.preventDefault();
